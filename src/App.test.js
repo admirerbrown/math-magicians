61b88d0dd4381e438077e669124e6db6544a2600
admirerbrown <admirerbrown@gmail.com>
2023-01-09 17:35:50 +0000

    fix linter issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-vars */
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(< App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-unresolved */
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client.js';
+import './index.css.js';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
