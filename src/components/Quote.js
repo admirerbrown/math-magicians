1afc7786223f8ee2ee900bb8280fa1d6234bd35a
admirerbrown <admirerbrown@gmail.com>
2023-01-17 17:56:41 +0000

    build full webpsite and add css to improve UI look

---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+const Quotes = () => (
+  <div className="q-div">
+    <p className="q-pageText">
+      Somehow it’s okay for people to chuckle about not being good at math. Yet,
+      if I said “I never learned to read,” they’d say I was an illiterate dolt.
+      —Neil deGrasse Tyson, American astrophysicist and author
+    </p>
+  </div>
+
+);
+
+export default Quotes;
