702cbab414aa5ebd0f408459c3d6d9e3eb09dfca
admirerbrown <admirerbrown@gmail.com>
2023-01-18 14:45:34 +0000

    add tests for all react components

--- src/tests/Calculate.test.js
+++ src/tests/Calculate.test.js
@@ -8,14 +8,14 @@ describe('test calculate function', () => {
     expect(output.total).toBe('3');
   });
 
-    test('it should return output', () => {
+  test('it should return output', () => {
     const obj = { total: '6', next: '2', operation: '+' };
     const buttonName = '=';
     const output = calculate(obj, buttonName);
     expect(output.total).toBe('8');
   });
 
-    test('it should return output', () => {
+  test('it should return output', () => {
     const obj = { total: '8', next: '2', operation: '-' };
     const buttonName = '=';
     const output = calculate(obj, buttonName);
